5ad94ffdebeacfdd6def34f25f849e34dc5a34bf
Kyle N <kyle.n.cs04@gmail.com>
2026-04-28 15:44:39 -0600

    Completed DOM practice activity page.

---
+++ ponder/scripts/dom.js
@@ -0,0 +1,33 @@
+const title = document.querySelector('h1');
+
+console.log(title);
+
+title.textContent = 'Web Page Components';
+
+//document.querySelector('#topics').style.color = 'red';
+
+document.getElementById('topics').style.color = 'purple';
+
+let list = document.querySelector('.list');
+
+list.style.border = '3px solid black';
+
+let para = document.querySelector('p');
+
+//para.style.backgroundColor = 'lightblue';
+
+para.classList.add('background');
+
+//document.querySelector('body').classList.add("background");
+
+const image = document.querySelector('img');
+image.setAttribute('src', 'images/web.png');
+
+
+let selectElem = document.getElementById('webdevlist');
+selectElem.addEventListener('change', function(){
+    let codeValue = selectElem.value;
+    console.log(codeValue);
+    document.getElementById(codeValue).style.color = 'red';
+})
+                
